8d51c3eba2495c644f527f883aa3ecc11c884af6
yearn1208 <739125798@qq.com>
2025-01-23 17:48:48 +0800

    [feat]

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,11 @@ module.exports = defineConfig({
     compress: true, //启动devServer的Gzip压缩
   },
 
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/mtt/" // 注意路径末尾的斜杠是必须的
-      : "/",
+  // publicPath:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/mtt/" // 注意路径末尾的斜杠是必须的
+  //     : "/",
+  publicPath: "/mtt/",
   //打包时输出文件目录
   outputDir: "dist",
   // //可写可不写，写的话js,css等文件会存在这个文件夹里面,放置静态文件夹目录
