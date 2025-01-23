08681973227b81662e9b9cfd0a90230c983cb68d
yearn1208 <739125798@qq.com>
2025-01-23 17:03:59 +0800

    test 2

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
 
   publicPath: "./",
   //打包时输出文件目录
-  outputDir: "as",
+  outputDir: "dist",
   // //可写可不写，写的话js,css等文件会存在这个文件夹里面,放置静态文件夹目录
   // assetsDir: "static",
 });
